0d0a7b47beead945767ee92a1127d4341f03f734
Simonasss <slapta.simas@gmai.com>
2021-02-27 16:51:56 +0200

    first commit

---
+++ include/links.js
@@ -0,0 +1,12 @@
+function myLink1 (){
+    window.open( 'https://nepatoguskinas.lt/2020/filmas/sugauti-tinkle-jaunimo-versija/?fbclid=IwAR3OFztioCpQ2QrtB-o4aKDvGoTIrqyBU4L60bl362XrirwrQjwsavDjDmk' ,  '_blank');
+}
+function myLink2 (){
+    window.open( 'https://manoit.jimdofree.com/saugus-internetas/ ' ,  '_blank');
+}
+function myLink3 (){
+    window.open( 'http://www.elektronika.lt/patarimai/ivairus/71328/saugus-internete-virtualaus-gyvenimo-taisykles-jaunuoliui/' ,  '_blank');
+}
+function myLink4 (){
+    window.open( 'https://www.youtube.com/watch?v=DchfEuUo4fk&ab_channel=BibliotekosPazangai' ,  '_blank');
+}
